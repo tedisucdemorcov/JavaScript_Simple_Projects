8465dda679cd3bb06fdfdd09a9e44489893f17d7
Calcii Cristian Teodor <110901389+tedisucdemorcov@users.noreply.github.com>
2023-05-30 11:16:53 +0300

    Basics JS
    News:
    - alert
    - consolo.log();
    - document.getElementById();

---
+++ Hello World/JS/script.js
@@ -0,0 +1,31 @@
+// alert!
+
+alert("hello world");
+alert(" alert it is special concept for adding some information before the page will load");
+alert("after alerts ends you can see the page.");
+// console log 
+console.log("consol.log() it is a method that provide permison to add some messages.");
+console.log("we will not see in the page but we can acces in inspect page -> console in browser");
+
+// Practice
+
+// 1.) Write a program that shows your name on the console.
+// 2.) Write a program that shows your address on the console.
+// 3.) Write a program that shows your name on alert.
+// 4.) Write a program that shows your address on alert.
+
+console.log("Teodor Cristian");
+console.log("i live in Brasov");
+console.log(alert("Teodor"));
+alert("Brasov");
+
+
+// document.getElementById
+
+console.log("document.hetElementById retruns an element with specific value.");
+console.log( "we must specify in HTML id of an element.");
+
+// document.getElementById("container_id").style.color="red";
+// later we will add this command here
+
+
